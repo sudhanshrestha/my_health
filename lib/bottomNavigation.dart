10a5980d25f4f9507886b6458ad6c947537221f5
Sudhan Shrestha <sudhanbshrestha@gmail.com>
2021-02-15 03:06:13 +0545

    Navigation created!

--- lib/bottomNavigation.dart
+++ lib/bottomNavigation.dart
@@ -2,11 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:my_health/pages/home/home.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:my_health/pages/medicine/medicine.dart';
 
-
-
+int selectedIndex = 0;
 
 //Custom
 class BottomNavigation extends StatefulWidget {
@@ -14,38 +12,72 @@ class BottomNavigation extends StatefulWidget {
   _BottomNavigationState createState() => _BottomNavigationState();
 }
 
-enum BottomIcons { Home, Medicine, Measurement, Profile }
-
 class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    BottomIcons bottomIcons = BottomIcons.Home;
     return BottomAppBar(
       child: Container(
-        padding: EdgeInsets.only(left: 24, right: 24, bottom: 30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(38.0),
+            topLeft: Radius.circular(38.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 5,
+                offset: Offset(0.0, 0.75)),
+          ],
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            BottomBar(
-                onPressed: () {
-                  setState(() {
-                    bottomIcons = BottomIcons.Home;
-                  });
-                },
-                bottomIcons: bottomIcons == BottomIcons.Home ? true : false,
-                icons: MdiIcons.home,
-                text: "Home"
+            // NavBarItems(MdiIcons.home,Colors.purple,true,0,),
+            // NavBarItems(MdiIcons.pill,Colors.indigo,false,1,),
+            // NavBarItems(MdiIcons.heart,Colors.pink,false,2,),
+            // NavBarItems(Icons.person,Colors.teal,false,3,),
+            NavBarItem(
+              iconName: MdiIcons.home,
+              colorName: Colors.purple,
+              isActive: true,
+              index: 0,
+              onPress: (){
+                selectedIndex = 0;
+                Navigator.push(context, new MaterialPageRoute(
+                    builder: (context) =>
+                    new HomePage())
+                );
+              },
             ),
-            BottomBar(
-                onPressed: () {
-                  setState(() {
-                    bottomIcons = BottomIcons.Medicine;
-                  });
-                },
-                bottomIcons:
-                bottomIcons == BottomIcons.Medicine ? true : false,
-                icons: MdiIcons.pill,
-                text: "Medicine"),
+            NavBarItem(
+              iconName: MdiIcons.pill,
+              colorName: Colors.indigo,
+              isActive: false,
+              index: 1,
+              onPress: (){
+                selectedIndex = 1;
+                Navigator.push(context, new MaterialPageRoute(
+                    builder: (context) =>
+                    new MedicinePage())
+                );
+              },
+            ),
+            NavBarItem(
+              iconName: MdiIcons.heart,
+              colorName: Colors.pink,
+              isActive: false,
+              index: 2,
+              onPress: (){},
+            ),
+            NavBarItem(
+              iconName: Icons.person,
+              colorName: Colors.teal,
+              isActive: false,
+              index: 3,
+              onPress: (){},
+            ),
           ],
         ),
       ),
@@ -53,54 +85,101 @@ class _BottomNavigationState extends State<BottomNavigation> {
   }
 }
 
-class BottomBar extends StatefulWidget {
-  Function onPressed;
-  bool bottomIcons;
-  String text;
-  IconData icons;
+class NavBarItem extends StatefulWidget {
+  NavBarItem(
+      {this.iconName, this.colorName, this.isActive, this.index, this.onPress});
 
-  BottomBar(
-      {@required this.onPressed,
-      @required this.bottomIcons,
-      @required this.icons,
-      @required this.text});
+  final IconData iconName;
+  final Color colorName;
+  final bool isActive;
+  final int index;
+  final Function onPress;
 
   @override
-  _BottomBarState createState() => _BottomBarState();
+  _NavBarItemState createState() => _NavBarItemState();
 }
 
-class _BottomBarState extends State<BottomBar> {
+class _NavBarItemState extends State<NavBarItem> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: widget.onPressed,
-        child: widget.bottomIcons == true
-            ? Container(
-                decoration: BoxDecoration(
-                  color: Colors.indigo.shade100.withOpacity(0.6),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding:
-                    EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      widget.icons,
-                      color: Colors.indigo,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      widget.text,
-                      style: TextStyle(
-                          color: Colors.indigo,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
-                    ),
-                  ],
-                ),
-              )
-            : Icon(widget.icons));
+      onTap: widget.onPress,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 4,
+        decoration: BoxDecoration(
+          color: widget.index == selectedIndex
+              ? widget.colorName.withOpacity(0.6)
+              : Colors.white,
+          borderRadius:BorderRadius.only(
+            topLeft:  Radius.circular(30),
+            topRight:  Radius.circular(30),
+          ),
+        ),
+        padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 15),
+        child: Padding(
+          padding: const EdgeInsets.only(top:8.0),
+          child: Icon(
+            widget.iconName,
+            color: widget.colorName,
+          ),
+        ),
+      ),
+    );
   }
 }
+
+// class BottomBar extends StatefulWidget {
+//   Function onPressed;
+//   bool bottomIcons;
+//   String text;
+//   IconData icons;
+//
+//   BottomBar(
+//       {@required this.onPressed,
+//       @required this.bottomIcons,
+//       @required this.icons,
+//       @required this.text});
+//
+//   @override
+//   _BottomBarState createState() => _BottomBarState();
+// }
+//
+// class _BottomBarState extends State<BottomBar> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//         onTap: widget.onPressed,
+//         child: widget.bottomIcons == true
+//             ? Container(
+//                 decoration: BoxDecoration(
+//                   color: Colors.indigo.shade100.withOpacity(0.6),
+//                   borderRadius: BorderRadius.circular(30),
+//                 ),
+//                 padding:
+//                     EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+//                 child: Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       widget.icons,
+//                       color: Colors.indigo,
+//                     ),
+//                     SizedBox(
+//                       width: 8,
+//                     ),
+//                     Text(
+//                       widget.text,
+//                       style: TextStyle(
+//                           color: Colors.indigo,
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 15),
+//                     ),
+//                   ],
+//                 ),
+//               )
+//             : Icon(widget.icons)
+//
+//
+//
+//     );
+//   }
+// }
